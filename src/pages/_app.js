82782a449dc3669e4cc0a4cffefb9c5f3ad397c9
Anthony Du <github@anthonydu.com>
2023-01-21 14:46:25 -0800

    Initialize Next.js, Home + NavBar complete

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import SideBar from '@/components/SideBar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProSidebarProvider>
+      <SideBar style={{ display: "fixed", top: 0, left: 0 }} />
+      <Component {...pageProps} />
+    </ProSidebarProvider>
+  );
 }
